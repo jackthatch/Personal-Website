89d549305eb48cf8c54df54bb31bcde4977db12a
jackthatch <jthatcher128@gmail.com>
2023-06-12 21:34:29 -0500

    Create Post Form (incomplete)

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,19 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { PrismaClient } from "@prisma/client";
+
 // and what to do when importing types
 declare namespace App {
 	// interface Locals {}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
+}
+
+declare global {
+	namespace App {
+
+	}
+	var prisma: PrismaClient
 }
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,40 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { AppBar, AppShell, Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<AppBar>
+			<svelte:fragment slot="lead">Jack's Personal Page</svelte:fragment>
+			
+			<svelte:fragment slot="default">
+
+					<div class="font-bold p-4 space-x-10">
+						<a href="blog"> Blog </a>
+						<a href="new-post"> Create Post </a>
+					</div>
+
+				
+				
+				
+			</svelte:fragment>
+			
+			<svelte:fragment slot="trail">
+				<Avatar
+				border="border-4 border-surface-300-600-token hover:!border-primary-500"
+				cursor="cursor-pointer"
+				/>
+			</svelte:fragment>
+		</AppBar>
+		</svelte:fragment>
+
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- (pageFooter) -->
+	<!-- (footer) -->
+</AppShell>
