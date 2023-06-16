4444809864b957aba7a26c128665acdd7e4ed9d7
jackthatch <jthatcher128@gmail.com>
2023-06-15 23:06:40 -0500

    Prisma Allow LargeText

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter(),
-
-    // Increase the maxRequestBodySize
-    server: {
-      middleware: [
-        // Custom middleware for increasing maxRequestBodySize
-        (req, res, next) => {
-          req.server.request.connection.server.maxHeadersCount = 100;
-          req.server.request.connection.server.maxHeaderSize = 32768;
-          req.server.request.connection.server.maxBodySize = 104857600;
-          next();
-        }
-      ]
-    }
-  }
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+	
+	vitePlugin: {
+		inspector: true,   
+	},
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
-
 export default config;
