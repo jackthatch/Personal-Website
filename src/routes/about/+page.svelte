e8d98e3b8a56bdacca45716ff9f025c8321319c8
jackthatch <jthatcher128@gmail.com>
2023-06-26 23:11:44 -0500

    Updatiing about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,45 +1,57 @@
 
+<main class="p-4">
 
-<main>
+	<div class="card p-4 text-center my-4">
+		<h1> Currently Working With: </h1>
+	</div>	
 
-<div class="card p-4 text-center my-4">
-	<h1> Currently Working With: </h1>
-</div>	
+	<div class='card p-4 text-center my-2'>
+		<h1> Python </h1>
+		<img class='w-24 h-24' src='https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/python.png' alt='Svelte-Icon'>
+		<p> Add Links to Python projects here </p>
+	</div>
 
-<div class='card p-4 text-center'>
-	<h1> Sveltekit </h1>
-	<img class='w-24 h-24' src='https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Svelte_Logo.svg/1200px-Svelte_Logo.svg.png', alt='Svelte-Icon'>
-	<p> Add Links to SvelteKit projects here </p>
-</div>
+	<div class='card p-4 text-center my-2'>
+		<h1> Sveltekit </h1>
+		<img class='w-24 h-24' src='https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/svelte.png' alt='Svelte-Icon'>
+		<p> Add Links to SvelteKit projects here </p>
+	</div>
+
+	<div class='card p-4 text-center my-2'>
+		<h1> TypeScript </h1>
+		<img class='w-24 h-24' src='https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/ts.png' alt='Svelte-Icon'>
+		<p> Add Links to TS projects here </p>
+	</div>
+
+
+	<div class='card p-4 text-center my-2'>
+		<h1> PostgreSQL </h1>
+		<img class='w-24 h-24' src='https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/pgsql.svg' alt='Svelte-Icon'>
+		<p> Add Links to PostgreSQL projects here </p>
+	</div>
+
+	<div class='card p-4 text-center my-8'>
+		
+		<h1> Want To Work With: </h1>
+		<img class='w-24 h-24' src="https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/go.png" alt="GO-Logo">
+
+	</div>
+
+	<div class='card p-4 text-center my-8 flex'>
+
+		<h1 class="p-4"> Operating Systems: </h1>
+		<img class='w-48 h-48 p-4' src="https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/windows.png" alt="Windows-Logo">
+		<img class='w-48 h-48 p-4' src="https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/arch1.png" alt="Arch-Logo">
 	
+	</div>
 
-<div class='card p-4 text-center'>
-	<h1> Python </h1>
-	<img class='w-24 h-24' src='https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg', alt='Svelte-Icon'>
-	<p> Add Links to Python projects here </p>
-</div>
+	<div class='card p-4 text-center my-8 flex'>
+		
+		<h1 class="p-4"> Tools and Other: </h1>
+		<img class='w-48 h-48 p-4' src="https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/supabase.jpg" alt="Supabase-Logo">
+		<img class='w-48 h-48 p-4' src="https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/git.png" alt="Git-Logo">
+		<img class='w-48 h-48 p-4' src="https://xcdezfzlpwnkrukbaxsi.supabase.co/storage/v1/object/public/Images/nvim.png" alt="Git-Logo">
+	</div>
 	
-
-<div class='card p-4 text-center'>
-	<h1> TypeScript </h1>
-	<img class='w-24 h-24' src='https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg', alt='Svelte-Icon'>
-	<p> Add Links to TS projects here </p>
-</div>
-
-
-<div class='card p-4 text-center'>
-	<h1> PostgreSQL </h1>
-	<img class='w-24 h-24' src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png', alt='Svelte-Icon'>
-	<p> Add Links to PostgreSQL projects here </p>
-</div>
-
-
-<div class='card p-4 text-center my-4'>
-	
-	<h1> Want To Work With: </h1>
-	<img class='w-48 h-24' src='https://www.pngitem.com/pimgs/m/11-116892_go-icon-png-small-transparent-png.png', alt="Go-Image">
-
-</div>
-
 
 </main>
