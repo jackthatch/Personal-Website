3145796a26fd64361e8ee05e0d8ebeb39457274d
jackthatch <jthatcher128@gmail.com>
2023-07-27 17:30:05 -0500

    even even more small mobile changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,12 @@
 <div class="container mx-auto flex justify-center my-10 p-4">
 	<div class="card p-4 text-center my-2 w-full max-w-screen-lg mx-auto variant-ghost-tertiary">
 		<h1 class="h3 font-semibold"> Welcome ! </h1>
-		<p class="p-4"> My name is Jackson Thatcher and this is my personal website </p>
+		<p class="p-4"> 
+			My name is Jackson Thatcher and this is my personal website. <br>
+			Navigate from the menu at the top of the page.
+			
+
+		</p>
 		<p class="font-semibold"> Feel free to post and view it in the blog tab! </p>
 	</div>
 </div>
