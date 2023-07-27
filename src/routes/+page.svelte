3eb5b0514ff5a069bcfe8cadcfd3b82a6faa395b
jackthatch <jthatcher128@gmail.com>
2023-07-27 17:52:13 -0500

    emojies (bc fast)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,9 @@
 	<div class="card p-4 text-center my-2 w-full max-w-screen-lg mx-auto variant-ghost-tertiary">
 		<h1 class="h3 font-semibold"> Welcome ! </h1>
 		<p class="p-4"> 
-			My name is Jackson Thatcher and this is my personal website. <br>
-			Navigate from the menu at the top of the page.
-			
-
+			⚡ My name is Jackson Thatcher and this is my personal website. <br><br>
+			🐱‍💻 Navigate from the menu at the top of the page.
 		</p>
-		<p class="font-semibold"> Feel free to post and view it in the blog tab! </p>
+		<p class="font-semibold"> Feel free to make a post and view it in the blog tab! </p>
 	</div>
 </div>
